606371d4d28d52bc6a2e8ed797168727d53828c8
Mananchery Panneerselvam <I29140@verisk.com>
2020-07-27 20:55:20 -0600

    Made changes and added node module to the files

--- router/user.js
+++ router/user.js
@@ -1,27 +1,16 @@
 import express from "express";
+import {v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
 
-const user = [
-    {
-        firstName: "Hitesh Saai",
-        lastName: "Mananchery",
-        age: 25,
-        occupation: "Software Engineer"
-    },
-    {
-        firstName: "John",
-        lastName: "Mick",
-        age: 25,
-        occupation: "Data Scientist"
-    }
-]
+const users = []
 
 // all routes over here are starting with /users for '/'
 router.get('/', (req ,res ) =>{
 
-    res.send(user);
+    res.send(users);
 
 } )
 
@@ -29,10 +18,45 @@ router.get('/', (req ,res ) =>{
 router.post('/', (req, res)=> {
 
     const newUser = req.body;
-    user.push(newUser)
+
+    const newUserWithID = {... newUser, id: uuidv4()};
+
+    users.push(newUserWithID);
+
     res.send(`Added new employee ${newUser['firstName']} as ${newUser['occupation']} and saved to Company Database!`);
 
 
 });
 
+// Getting the User profile using the unique id of the user, and /: reterives everything after that
+router.get('/:id', (req, res) => {
+
+    const {id} =  req.params;
+    console.log(req.params);
+
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+})
+
 export default router;
+
+
+
+// {
+//     "firstName": "Hitesh Saai",
+//     "lastName": "Mananchery",
+//     "age": 25,
+//     "occupation": "Software Engineer"
+// },
+// {
+//     "firstName": "John",
+//     "lastName": "Mick",
+//     "age": 25,
+//     "occupation": "Data Scientist"
+// },
+// {
+//     "firstName": "john",
+//     "lastName": "den",
+//     "age": 22,
+//     "occupation": "Data Analyst"
+// }
